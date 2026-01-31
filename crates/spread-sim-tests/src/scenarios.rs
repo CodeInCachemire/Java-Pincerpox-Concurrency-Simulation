7f2dbdf78c7f8ad1693e26371711840d08588738
Ahmad Afroz Qureshi <ahqu00001@stud.uni-saarland.de>
2026-01-31 02:57:39 +0100

    Delete Useless Files

--- crates/spread-sim-tests/src/scenarios.rs
+++
@@ -1,7 +0,0 @@
-//! Test scenarios.
-
-use crate::test_scenario_public;
-
-// This macro defines a test scenario located in `scenarios/public`.
-// You may add your own scenarios here.
-test_scenario_public!(WE_LOVE_NP, "we_love_np");
--- crates/spread-sim-tests/src/tests/test_slug.rs
+++
@@ -1,6 +0,0 @@
-use crate::scenarios;
-
-#[test]
-fn test_slug() {
-    scenarios::WE_LOVE_NP.test_case().creep()
-}
